71fa9b0db25717ac1117595e8ef263bb09c5dbbb
James Furtado <jamesdfurtado@gmail.com>
2025-08-12 11:15:21 -0400

    Added conditional light indicator component

---
+++ src/ui/widgets/Indicator.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../Widget.h"
+#include <string>
+
+class Indicator : public Widget {
+public:
+    explicit Indicator(const std::string& label, bool initialState = false)
+        : label(std::move(label)), isActive(initialState) {}
+
+    void setState(bool active) { isActive = active; }
+    bool getState() const { return isActive; }
+    void setLabel(const std::string& newLabel) { label = newLabel; }
+
+    void draw() const override {
+        // Draw rectangular box outline
+        DrawRectangleLinesEx(bounds, 1, LIGHTGRAY);
+        
+        // Draw lightbulb circle (red when false, green when true)
+        Color lightColor = isActive ? GREEN : RED;
+        int centerX = (int)(bounds.x + 12);
+        int centerY = (int)(bounds.y + bounds.height / 2);
+        DrawCircle(centerX, centerY, 8, lightColor);
+        
+        // Draw text label
+        DrawText(label.c_str(), (int)(bounds.x + 28), (int)(bounds.y + 6), 16, RAYWHITE);
+    }
+
+private:
+    std::string label;
+    bool isActive;
+};
