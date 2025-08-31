93708a09c42944f794475c46c0c0f1805cd0e2a8
James Furtado <jamesdfurtado@gmail.com>
2025-08-31 15:28:24 -0400

    launch phase handling

--- src/sim/systems/LaunchSequenceHandler/LaunchingPhase.h
+++ src/sim/systems/LaunchSequenceHandler/LaunchingPhase.h
@@ -6,9 +6,6 @@
 
 class LaunchingPhase {
 public:
-    // Check if launching is complete and we can transition to Launched
     static bool isLaunchingComplete(float launchingTimer);
-    
-    // Get the launching progress message
     static std::string getLaunchingMessage(float launchingTimer);
 };
