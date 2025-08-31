81780547537954d93392e7b3d0606e60c5af4743
James Furtado <jamesdfurtado@gmail.com>
2025-08-31 15:50:52 -0400

    file clean-up

--- src/sim/systems/EnvironmentSystem.h
+++ src/sim/systems/EnvironmentSystem.h
@@ -6,7 +6,6 @@ class EnvironmentSystem : public ISystem {
 public:
     const char* getName() const override { return "EnvironmentSystem"; }
     void update(SimulationState& state, float /*dt*/) override {
-        // Keep it simple for now
         state.launchConditionsFavorable = true;
         state.launchTubeIntegrity = true;
 
