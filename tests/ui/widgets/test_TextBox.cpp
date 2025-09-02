9858ce130bc0a4a54f840f086d1b6b45c888ae12
James Furtado <jamesdfurtado@gmail.com>
2025-09-01 22:53:50 -0400

    test: clear textbox content completely

--- tests/ui/widgets/test_TextBox.cpp
+++ tests/ui/widgets/test_TextBox.cpp
@@ -3,27 +3,63 @@
 
 class TextBoxTest : public ::testing::Test {
 protected:
+    TextBox textBox;
+    
     void SetUp() override {
-        // Set up text box testing
+        textBox = TextBox(TextBox::Mode::INPUT, 4);
+        textBox.setBounds({10, 10, 100, 30});
     }
 };
 
-TEST_F(TextBoxTest, DefaultState) {
-    // TODO: Test default text box state
-    EXPECT_TRUE(true); // Placeholder
+TEST_F(TextBoxTest, InitializesWithCorrectMode) {
+    TextBox displayBox(TextBox::Mode::DISPLAY, 6);
+    TextBox inputBox(TextBox::Mode::INPUT, 4);
+    
+    EXPECT_EQ(displayBox.getMode(), TextBox::Mode::DISPLAY);
+    EXPECT_EQ(inputBox.getMode(), TextBox::Mode::INPUT);
 }
 
-TEST_F(TextBoxTest, TextInput) {
-    // TODO: Test text input functionality
-    EXPECT_TRUE(true); // Placeholder
+TEST_F(TextBoxTest, AddCharacterRespectsMaxLength) {
+    textBox.addCharacter('1');
+    textBox.addCharacter('2');
+    textBox.addCharacter('3');
+    textBox.addCharacter('4');
+    textBox.addCharacter('5');
+    
+    EXPECT_EQ(textBox.getText(), "1234");
+    EXPECT_EQ(textBox.getCurrentLength(), 4);
+    EXPECT_TRUE(textBox.isComplete());
 }
 
-TEST_F(TextBoxTest, TextDisplay) {
-    // TODO: Test text display
-    EXPECT_TRUE(true); // Placeholder
+TEST_F(TextBoxTest, AddCharacterOnlyAcceptsDigits) {
+    textBox.addCharacter('a');
+    textBox.addCharacter('1');
+    textBox.addCharacter('b');
+    textBox.addCharacter('2');
+    
+    EXPECT_EQ(textBox.getText(), "12");
+    EXPECT_EQ(textBox.getCurrentLength(), 2);
 }
 
-TEST_F(TextBoxTest, KeyboardInput) {
-    // TODO: Test keyboard input handling
-    EXPECT_TRUE(true); // Placeholder
+TEST_F(TextBoxTest, RemoveCharacterWorks) {
+    textBox.addCharacter('1');
+    textBox.addCharacter('2');
+    textBox.addCharacter('3');
+    
+    textBox.removeCharacter();
+    
+    EXPECT_EQ(textBox.getText(), "12");
+    EXPECT_EQ(textBox.getCurrentLength(), 2);
+}
+
+TEST_F(TextBoxTest, ClearEmptiesTextBox) {
+    textBox.addCharacter('1');
+    textBox.addCharacter('2');
+    textBox.addCharacter('3');
+    
+    textBox.clear();
+    
+    EXPECT_TRUE(textBox.isEmpty());
+    EXPECT_EQ(textBox.getCurrentLength(), 0);
+    EXPECT_EQ(textBox.getText(), "");
 }
