e2ce54dc264d9935909d3c77cb4c93359e2f6f9e
James Furtado <jamesdfurtado@gmail.com>
2025-09-01 22:53:52 -0400

    test: update throttle value on mouse drag

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,10 +1,8 @@
 #include <gtest/gtest.h>
 
-// Test main runner
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     
-    // Run all tests
     int result = RUN_ALL_TESTS();
     
     return result;
